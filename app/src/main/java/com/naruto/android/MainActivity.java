4496966c09e0eeb6147f85f04de0ba662ba46cf8
HanXuanMo <a447899372@163.com>
2018-08-30 00:18:11 +0800

    底部导航栏

--- app/src/main/java/com/naruto/android/MainActivity.java
+++ app/src/main/java/com/naruto/android/MainActivity.java
@@ -1,13 +1,47 @@
 package com.naruto.android;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.home_page:
+                    mTextMessage.setText(R.string.title_home_page);
+                    return true;
+                case R.id.navigation_game:
+                    mTextMessage.setText(R.string.title_game);
+                    return true;
+                case R.id.navigation_circle:
+                    mTextMessage.setText(R.string.title_circle);
+                    return true;
+                case R.id.navigation_me:
+                    mTextMessage.setText(R.string.title_me);
+                    return true;
+                default:
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
