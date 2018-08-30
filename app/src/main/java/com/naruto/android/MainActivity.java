347554865b2ef9d9eba83f8cb4f2eba5c9b530a5
HanXuanMo <a447899372@163.com>
2018-08-30 09:31:20 +0800

    顶部导航栏

--- app/src/main/java/com/naruto/android/MainActivity.java
+++ app/src/main/java/com/naruto/android/MainActivity.java
@@ -1,16 +1,19 @@
 package com.naruto.android;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
+    //底部导航栏
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -18,16 +21,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.home_page:
-                    mTextMessage.setText(R.string.title_home_page);
+
                     return true;
                 case R.id.navigation_game:
-                    mTextMessage.setText(R.string.title_game);
+
                     return true;
                 case R.id.navigation_circle:
-                    mTextMessage.setText(R.string.title_circle);
+
                     return true;
                 case R.id.navigation_me:
-                    mTextMessage.setText(R.string.title_me);
+
                     return true;
                 default:
             }
@@ -40,8 +43,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        //底部导航栏
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Button button = (Button) findViewById(R.id.Intent);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewPageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
