348ce39acfb3332e5d95d63fc60539a0ec13ac83
HanXuanMo <a447899372@163.com>
2018-08-30 10:35:56 +0800

    -底部导航栏+滑动菜单

--- app/src/main/java/com/naruto/android/MainActivity.java
+++ app/src/main/java/com/naruto/android/MainActivity.java
@@ -1,59 +1,18 @@
 package com.naruto.android;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    //底部导航栏
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.home_page:
-
-                    return true;
-                case R.id.navigation_game:
-
-                    return true;
-                case R.id.navigation_circle:
-
-                    return true;
-                case R.id.navigation_me:
-
-                    return true;
-                default:
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //底部导航栏
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.home_page);
+        setSupportActionBar(toolbar);
 
-        Button button = (Button) findViewById(R.id.Intent);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ViewPageActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
