0b3bb46daa2c6cbe367abbd4dd3118d52060d999
YHTION <2380126648@qq.com>
2018-08-31 11:37:12 +0800

    集合未完成

--- app/src/main/java/com/naruto/android/MainActivity.java
+++ app/src/main/java/com/naruto/android/MainActivity.java
@@ -1,7 +1,9 @@
 package com.naruto.android;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -25,6 +27,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //按钮距离
+    private static final int DISTANCE = 300;
+    private static final int DISTANCE2 = 220;
+
+    private FloatingActionButton actionButton, actionButton1, actionButton2, actionButton3;
+    private boolean mMenuOpen = false;
+    private View mFlMenu;
+
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
     private ViewPager mViewPager;
@@ -36,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
 
         //滑动菜单点击事件
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);//获取滑动菜单事件
@@ -92,6 +103,190 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+    }
+
+
+    private void initView(){
+        mFlMenu = findViewById(R.id.fl_menu);
+
+        actionButton = (FloatingActionButton) findViewById(R.id.float_btn);
+        actionButton1 = (FloatingActionButton) findViewById(R.id.float_btn1);
+        actionButton2 = (FloatingActionButton) findViewById(R.id.float_btn2);
+        actionButton3 = (FloatingActionButton) findViewById(R.id.float_btn3);
+
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMenuOpen) {
+                    hideMenu();
+                }else {
+                    showMenu();
+                }
+            }
+        });
+        //触发按钮
+        actionButton1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //获取原按钮及点击按钮图片
+                Drawable drawable = actionButton.getDrawable();
+                Drawable drawable1 = actionButton1.getDrawable();
+                //交换原按钮及点击按钮图片
+                actionButton1.setImageDrawable(drawable);
+                actionButton.setImageDrawable(drawable1);
+                hideMenu();
+            }
+        });
+
+        actionButton2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Drawable drawable = actionButton.getDrawable();
+                Drawable drawable2 = actionButton2.getDrawable();
+                actionButton2.setImageDrawable(drawable);
+                actionButton.setImageDrawable(drawable2);
+                hideMenu();
+            }
+        });
+
+        actionButton3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Drawable drawable = actionButton.getDrawable();
+                Drawable drawable3 = actionButton3.getDrawable();
+                actionButton3.setImageDrawable(drawable);
+                actionButton.setImageDrawable(drawable3);
+                hideMenu();
+            }
+        });
+    }
+
+
+    private void showMenu() {
+        mMenuOpen = true;
+        //获取按钮坐标
+        int x = (int) actionButton.getX();
+        int y = (int) actionButton.getY();
+        //动画
+        ValueAnimator v1 = ValueAnimator.ofInt(x, x - DISTANCE);
+        //动画播放时间
+        v1.setDuration(500);
+        //动画更新
+        v1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                //获取按钮上，下，左，右的坐标
+                int l = (int) animation.getAnimatedValue();
+                int t = (int) actionButton1.getY();
+                int r = actionButton1.getWidth() + l;
+                int b = actionButton1.getHeight() + t;
+                //设置按钮坐标
+                actionButton1.layout(l, t, r, b);
+            }
+        });
+        ValueAnimator v2x = ValueAnimator.ofInt(x, x - DISTANCE2);
+        ValueAnimator v2y = ValueAnimator.ofInt(y, y - DISTANCE2);
+        v2x.setDuration(500).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int l = (int) animation.getAnimatedValue();
+                int t = (int) actionButton2.getY();
+                int r = actionButton2.getWidth() + l;
+                int b = actionButton2.getHeight() + t;
+                actionButton2.layout(l, t, r, b);
+            }
+        });
+        v2y.setDuration(500).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int t = (int) animation.getAnimatedValue();
+                int l = (int) actionButton2.getX();
+                int r = actionButton2.getWidth() + l;
+                int b = actionButton2.getHeight() + t;
+                actionButton2.layout(l, t, r, b);
+            }
+        });
+        ValueAnimator v3 = ValueAnimator.ofInt(y, y - DISTANCE);
+        v3.setDuration(500).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int t = (int) animation.getAnimatedValue();
+                int l = (int) actionButton3.getX();
+                int r = actionButton3.getWidth() + l;
+                int b = actionButton3.getHeight() + t;
+                actionButton3.layout(l, t, r, b);
+            }
+        });
+        //开始动画
+        v1.start();
+        v2x.start();
+        v2y.start();
+        v3.start();
+    }
+
+    private void hideMenu() {
+        mMenuOpen = false;
+        //获取按钮坐标
+        int x = (int) actionButton1.getX();
+        //动画
+        ValueAnimator v1 = ValueAnimator.ofInt(x, (int) actionButton.getX());
+        //动画播放时间
+        v1.setDuration(500);
+        //动画更新
+        v1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                //获取按钮上，下，左，右的坐标
+                int l = (int) animation.getAnimatedValue();
+                int t = (int) actionButton1.getY();
+                int r = actionButton1.getWidth() + l;
+                int b = actionButton1.getHeight() + t;
+                //设置按钮坐标
+                actionButton1.layout(l, t, r, b);
+            }
+        });
+        x = (int) actionButton2.getX();
+        int y = (int) actionButton2.getY();
+        ValueAnimator v2x = ValueAnimator.ofInt(x, (int) actionButton.getX());
+        ValueAnimator v2y = ValueAnimator.ofInt(y, (int) actionButton.getY());
+        v2x.setDuration(500).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int l = (int) animation.getAnimatedValue();
+                int t = (int) actionButton2.getY();
+                int r = actionButton2.getWidth() + l;
+                int b = actionButton2.getHeight() + t;
+                actionButton2.layout(l, t, r, b);
+            }
+        });
+        v2y.setDuration(500).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int t = (int) animation.getAnimatedValue();
+                int l = (int) actionButton2.getX();
+                int r = actionButton2.getWidth() + l;
+                int b = actionButton2.getHeight() + t;
+                actionButton2.layout(l, t, r, b);
+            }
+        });
+        y = (int) actionButton3.getY();
+        ValueAnimator v3 = ValueAnimator.ofInt(y, (int) actionButton.getY());
+        v3.setDuration(500).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int t = (int) animation.getAnimatedValue();
+                int l = (int) actionButton3.getX();
+                int r = actionButton3.getWidth() + l;
+                int b = actionButton3.getHeight() + t;
+                actionButton3.layout(l, t, r, b);
+            }
+        });
+        //开始动画
+        v1.start();
+        v2x.start();
+        v2y.start();
+        v3.start();
     }
 
     //菜单点击事件
@@ -128,24 +323,6 @@ public class MainActivity extends AppCompatActivity {
             View rootView = inflater.inflate(R.layout.content_main, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.text_view);
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            //Intent Ps
-            Button intentPs = (Button) rootView.findViewById(R.id.intent_ps);
-            intentPs.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(container.getContext(), Ps.class);
-                    startActivity(intent);
-                }
-            });
-            //Intent Login
-            Button intentLogin = (Button) rootView.findViewById(R.id.intent_login);
-            intentLogin.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(container.getContext(), Login.class);
-                    startActivity(intent);
-                }
-            });
             return rootView;
         }
     }
